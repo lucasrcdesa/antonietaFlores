2fecefaa2b9b98e567758989cdf46cddd0e40c09
Lucas Rodrigues <lucasrcdesa@hotmail.com>
2026-04-17 15:50:49 -0300

    alterando imagens mainProducts

--- front/src/components/mainProductsComponent/mainProducts.tsx
+++ front/src/components/mainProductsComponent/mainProducts.tsx
@@ -1,42 +1,44 @@
 import styles from "./mainProducts.module.css";
-import heroImage from "../../assets/hero.png";
-import florbackground from "../../assets/florbackground.png";
-import backgroundImage from "../../assets/background.jpg";
+import noivas from "../../assets/tiaComNoivas.jpeg";
+import eventos from "../../assets/eventosArranjo.jpeg";
+import caixa from "../../assets/caixaGG.jpeg";
+import mimo from "../../assets/mimo.jpeg";
+import buque from "../../assets/buqueG.jpeg";
 
 const cards = [
   {
     id: "sectionBuque",
     title: "Buquês",
     description: "Arranjos elegantes e delicados, perfeitos para presentear em qualquer ocasião.",
-    image: heroImage,
+    image: buque,
     reverse: false,
   },
   {
     id: "sectionMimo",
     title: "Mimos",
     description: "Pequenos gestos florais com grande impacto visual e muito charme.",
-    image: florbackground,
+    image: mimo,
     reverse: true,
   },
   {
     id: "sectionCaixa",
     title: "Caixas",
     description: "Combinações sofisticadas dentro de caixas especiais para surpreender.",
-    image: backgroundImage,
+    image: caixa,
     reverse: false,
   },
   {
     id: "sectionNoivas",
     title: "Noivas",
     description: "Detalhes florais exclusivos para tornar o seu dia ainda mais inesquecível.",
-    image: heroImage,
+    image: noivas,
     reverse: true,
   },
   {
     id: "sectionObs",
     title: "Eventos",
     description: "Decoração floral personalizada para eventos com estilo e elegância.",
-    image: florbackground,
+    image: eventos,
     reverse: false,
   },
 ];
