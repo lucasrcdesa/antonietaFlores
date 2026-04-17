cd3219d698f3d342a54baa139fdb029c00b89c36
Lucas Rodrigues <lucasrcdesa@hotmail.com>
2026-04-17 12:39:25 -0300

    retirando localhost

--- front/src/login/loginScreen.tsx
+++ front/src/login/loginScreen.tsx
@@ -22,7 +22,7 @@ const LoginScreen = () => {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:8080/auth/login', {
+            const response = await fetch('http://antonietaflores/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
