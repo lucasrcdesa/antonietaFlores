91ea762e8902808ffba97add2913149ebc68f07a
Lucas Rodrigues <lucasrcdesa@hotmail.com>
2026-06-08 13:29:41 -0300

    pdf ocupando a tela toda

--- front/src/pdfs/namoradosScreen.tsx
+++ front/src/pdfs/namoradosScreen.tsx
@@ -1,26 +1,14 @@
-import HomeHeader from "../home/homeHeader/homeHeader";
-import styles from "./namoradosScreen.module.css";
 import namoradosPdf from "./namorados.pdf?url";
 
 const NamoradosScreen = () => {
   return (
-    <div className={styles.pageContainer}>
-      <HomeHeader />
-
-      <div className={styles.pageHeader}>
-        <h1 className={styles.title}>Dia dos Namorados</h1>
-        <p className={styles.subtitle}>Catálogo especial de arranjos para a data mais romântica do ano</p>
-      </div>
-
-      <div className={styles.content}>
-        <iframe
-          src={namoradosPdf}
-          className={styles.pdfFrame}
-          title="Catálogo Dia dos Namorados"
-        />
-      </div>
-    </div>
+    <iframe
+      src={namoradosPdf}
+      style={{ position: "fixed", inset: 0, width: "100%", height: "100%", border: "none" }}
+      title="Catálogo Dia dos Namorados"
+    />
   );
 };
 
 export default NamoradosScreen;
+
